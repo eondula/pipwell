0c8d3fb11ddb3f24e71ba9f45e0a3da3a355d21e
eondula <leezmiriam@gmail.com>
2023-11-04 17:24:38 -0700

    added logging functionality

--- server.js
+++ server.js
@@ -13,8 +13,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use('/auth', authRoutes);
+// app.use('/auth', authRoutes);
 app.use(express.static('static'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/static/login.html');
+});
+
+app.get('/secret', (req, res) => {
+    res.sendFile(__dirname + '/static/secret-page.html');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
